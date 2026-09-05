10bf58c79074c7d0fb78431ae1e9057975fb0dd4
Stage4AE <bench@stage4ae>
2026-09-05 15:10:32 +0300

    feat: normalise question capitalisation

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ papers_folder = Path("papers")
 for paper in papers_folder.glob("*.txt"):
     with open(paper) as file:
         for line in file:
-            question = lines.strip()
+            question = lines.lower().strip()
             if question:
                 questions.append(question)
 
--- test.py
+++
@@ -1,6 +0,0 @@
-name = input("What is your name? ")
-
-print("Hello", name)
-
-for number in range(5):
-    print(number)
